032c8b05709aa5fec780526bf4e951a567dc4aac
fwsdbh <fwsdbh@outlook.com>
2025-12-02 20:45:33 +0800

    删除qml及一些暂时无关文件

--- src/Mirai_Pet.cpp
+++ src/Mirai_Pet.cpp
@@ -3,29 +3,11 @@
 #include <QApplication>
 #include <QMainWindow>
 
-//#include <QQmlApplicationEngine>
-//#include <QQuickWindow>
-//#include <Saba/Model/MMD/PMXModel.h>
-
 #include "PetWindow.h"
 
 int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
-
-    //// 启用透明窗口支持
-    //QQuickWindow::setDefaultAlphaBuffer(true);
-
-    //QQmlApplicationEngine engine;
-    //engine.loadFromModule("MyPet", "Main");
-
-    //auto pmdModel = std::make_unique<saba::PMXModel>();
-    //if (!pmdModel->Load("D:/WorkSpace/C++/Project_Code/Mirai_Pet/haila.pmx", "E:/MMd/haila"))
-    //{
-    //    std::cout << "Failed to load pmd file.\n";
-    //    return false;
-    //}
-
 
 	PetWindow win;
 	win.show();
